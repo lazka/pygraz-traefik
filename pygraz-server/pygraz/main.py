09646c4a1a5e012d2f684ba947d3a0da95e28b56
Christoph Reiter <reiter.christoph@gmail.com>
2021-11-01 17:11:02 +0100

    Initial commit

---
+++ pygraz-server/pygraz/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import httpx
+
+app = FastAPI()
+
+
+@app.get("/events")
+async def events():
+    async with httpx.AsyncClient() as client:
+        r = await client.get(
+            'https://api.meetup.com/PyGRAZ/events', timeout=10.0)
+        return r.json()
+
+
+@app.get("/test")
+async def test():
+    return {"name": "test"}
